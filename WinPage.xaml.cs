419b1fac7ca3ee77f9c791bcafe462c68ea1ad7b
AlexanDUHHH <bogoney@outlook.com>
2024-01-01 16:27:35 +0000

    Battle Boats Project File

---
+++ WinPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Battle_Boats
+{
+
+
+    /// <summary>
+    /// Interaction logic for WinPage.xaml
+    /// </summary>
+    public partial class WinPage : UserControl
+    {
+        
+        public WinPage(string winner)
+        {
+            InitializeComponent();
+            WinnerLabel.Content = $"{winner} is the winner";
+        }
+
+        private void ButtonClick_MainMenu(object sender, RoutedEventArgs e)
+        {
+            Window window = Window.GetWindow(this);
+            window.Content = new MainMenu(true);
+        }
+    }
+}
